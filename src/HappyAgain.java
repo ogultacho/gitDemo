460faaca9ab3d331f6bdcb3b58a7cc1492578a54
ogultacho <ogultacho@gmail.com>
2020-08-01 12:32:58 -0400

    add HappyAgain.java

---
+++ src/HappyAgain.java
@@ -0,0 +1,4 @@
+public class HappyAgain {
+    //i wrote some code
+
+}
--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,6 +1,7 @@
 public class HappyB20 {
     public static void main(String[] args) {
 
+        System.out.println("B20 is always happy");
 
 
         //a brand new line is added
